4a2faa9faf2a0e62acbc8bb1b23dd148875e6d4e
ghosttravislt <enochbaafi56@gmail.com>
2024-08-14 05:09:23 +0100

    first commit

---
+++ timer.js
@@ -0,0 +1,28 @@
+let displayTime = document.querySelector(".displaytime"),
+  timer = document.querySelector(".timer"),
+  time = document.querySelector("#time"),
+  text = document.querySelector("#text"),
+  btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  let actualValue = time.value;
+  let test = setInterval(() => {
+    if (actualValue !== "") {
+      actualValue--;
+      console.log(actualValue);
+      displayTime.innerText = actualValue;
+      btn.disabled = true;
+    } else {
+      clearInterval(test);
+    }
+
+    if (actualValue == 0) {
+      text.innerText = "Fuck you Nocky 👋";
+      btn.disabled = false;
+      clearInterval(test);
+    }
+    if (actualValue > 0) {
+      text.innerText = "Count down intiated";
+    }
+  }, 1000);
+});
